b173adeca8bdcf57b75a1e5050a516604f622b69
YUGESH-AE <yugeshae@gmail.com>
2023-08-20 16:04:49 +0530

    first commit

---
+++ src/main/java/com/example/springframework5/SpringFrameWork5Application.java
@@ -0,0 +1,13 @@
+package com.example.springframework5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameWork5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameWork5Application.class, args);
+	}
+
+}
