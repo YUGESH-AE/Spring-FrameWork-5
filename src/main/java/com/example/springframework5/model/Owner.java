9b0afb8a4356250fae498c0b85983278878ec17d
YUGESH-AE <yugeshae@gmail.com>
2023-08-22 10:29:48 +0530

    adding pojos

---
+++ src/main/java/com/example/springframework5/model/Owner.java
@@ -0,0 +1,4 @@
+package com.example.springframework5.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/example/springframework5/model/Vet.java
@@ -0,0 +1,5 @@
+package com.example.springframework5.model;
+
+public class Vet extends Person {
+
+}
